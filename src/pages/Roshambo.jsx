7e445ffd469417a6619bcdb496e18625f89d2788
rakafantino <rakafantino@gmail.com>
2024-05-31 20:13:55 +0700

    chore: so the roshambo component is more concise and readable

--- src/pages/Roshambo.jsx
+++ src/pages/Roshambo.jsx
@@ -1,24 +1,19 @@
 import { lazy, Suspense, useEffect, useState, useRef, useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import rock from "../assets/img/rock.png";
+import paper from "../assets/img/paper.png";
+import scissors from "../assets/img/scissors.png";
+import bgm from "../assets/bgm.wav";
+import useAlert from "../hooks/useAlert";
+import { getRandomInt, determineOutcome } from "../utils/gameLogic";
+
 const Options = lazy(() => import("../components/Options"));
 const Arena = lazy(() => import("../components/Arena"));
 const Header = lazy(() => import("../components/Header"));
 const Footer = lazy(() => import("../components/Footer"));
-import rock from "../assets/img/rock.png";
-import paper from "../assets/img/paper.png";
-import scissors from "../assets/img/scissors.png";
-import useAlert from "../hooks/useAlert";
-import { getRandomInt, determineOutcome } from "../utils/gameLogic";
-import Swal from "sweetalert2";
-import bgm from "../assets/bgm.wav";
 
 const Roshambo = () => {
-  useEffect(() => {
-    document.title = "Roshambo";
-  }, []);
-
-  const choices = useMemo(() => ["Rock", "Paper", "Scissors"], []);
-
   const [gameState, setGameState] = useState({
     selected: "Rock",
     cpuSelected: "Rock",
@@ -27,8 +22,10 @@ const Roshambo = () => {
   const audioRef = useRef(new Audio(bgm));
   const navigate = useNavigate();
   const showAlert = useAlert(navigate);
+  const choices = useMemo(() => ["Rock", "Paper", "Scissors"], []);
 
   useEffect(() => {
+    document.title = "Roshambo";
     const audio = audioRef.current;
     audio.loop = true;
 
@@ -54,7 +51,7 @@ const Roshambo = () => {
     };
   }, []);
 
-  const clicked = useCallback(
+  const handleClick = useCallback(
     (name) => {
       setGameState((prevState) => ({
         selected: name,
@@ -96,7 +93,7 @@ const Roshambo = () => {
         <div className="flex flex-row my-2 lg:my-5 w-full justify-center">
           <Suspense fallback={<div>Loading...</div>}>
             {choices.map((choice) => (
-              <Options key={choice} selected={choice} img={{ Rock: rock, Paper: paper, Scissors: scissors }[choice]} name={choice.toUpperCase()} onClick={clicked} />
+              <Options key={choice} selected={choice} img={{ Rock: rock, Paper: paper, Scissors: scissors }[choice]} name={choice.toUpperCase()} onClick={handleClick} />
             ))}
           </Suspense>
         </div>
