dd728c461867874ecd836a658ceafb436011d9f9
rakafantino <rakafantino@gmail.com>
2024-06-01 13:19:48 -0600

    fix: user can click more than 1 time

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
-const Options = ({ selected, img, name, onClick, disabled }) => (
+const Options = ({ selected, img, name, onClick, disabled, isSelected }) => (
   <button
     type="button"
     onClick={() => onClick(selected)}
-    className={`lg:p-6 sm:p-2 mx-2 basis-1/3 bg-white border border-gray-200 rounded-lg shadow focus:outline-none dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700 ${
-      disabled ? "cursor-not-allowed opacity-50" : ""
-    }`}
+    className={`lg:p-6 sm:p-2 mx-2 basis-1/3 bg-white border border-gray-200 rounded-lg shadow focus:outline-none dark:bg-gray-800 dark:border-gray-700 ${
+      disabled ? "cursor-not-allowed opacity-50" : "hover:scale-105 hover:shadow-lg hover:ring-4 hover:ring-yellow-400"
+    } ${isSelected ? "scale-105 shadow-lg ring-4 ring-yellow-400" : ""}`}
     disabled={disabled}
   >
     <h5 className="mb-2 lg:text-2xl sm:text-sm font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
@@ -20,10 +20,12 @@ Options.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
+  isSelected: PropTypes.bool,
 };
 
 Options.defaultProps = {
   disabled: false,
+  isSelected: false,
 };
 
 export default Options;
