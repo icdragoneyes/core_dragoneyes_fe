7cb48cc17c0d0926b5de5973ee4256032ebde66f
Felix Cary Giovanni <felix.cary@gmail.com>
2024-06-26 17:37:08 +0700

    feat: adding package and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-land': "url('/src/assets/spin_wheel/background.webp')",
+        'background-wheel': "url('/src/assets/spin_wheel/wheel.webp')",
+      },
+      fontFamily: {
+        sans: ["'Inter Variable'", ...defaultTheme.fontFamily.sans],
+        passero: ["'Passero One'"],
+        passion: ["'Passion One'"],
+      },
+      colors: {
+        "dark-blue": "#1E3557",
+        "bold-blue": "#0754C4",
+        "bright-red": "#EE5151",
+        "warning-yellow": "#FAC515",
+        "warm-white": "#FAF7EE",
+        "dark-cream": "#DDD9CB",
+        'primary-gray': '#C4BCC8'
+      },
+    },
   },
   plugins: [],
 };
