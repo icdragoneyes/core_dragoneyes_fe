a9ee8b83da3aa49e84da48dc9565e0bcac411df9
icdragoneyes <icdragoneyes@gmail.com>
2024-06-28 14:59:08 +0700

    add roshambo canisters

---
+++ src/IC/candid/roshambo.did.js
@@ -0,0 +1,67 @@
+export const idlFactory = ({ IDL }) => {
+  const Tokens = IDL.Record({ e8s: IDL.Nat64 });
+  const ClaimHistory = IDL.Record({
+    reward_claimed: IDL.Nat,
+    time: IDL.Int,
+    icp_transfer_index: IDL.Nat,
+  });
+  const Bet = IDL.Record({
+    id: IDL.Nat,
+    win: IDL.Bool,
+    time_created: IDL.Int,
+    betAmount: IDL.Nat,
+    eyes: IDL.Nat,
+    guess: IDL.Nat,
+    caller: IDL.Principal,
+    houseGuess: IDL.Nat,
+  });
+  const RoshamboUser = IDL.Record({
+    walletAddress: IDL.Principal,
+    claimableReward: IDL.Nat,
+    claimHistory: IDL.Vec(ClaimHistory),
+    betHistory: IDL.Vec(Bet),
+  });
+  const GameData = IDL.Variant({ ok: RoshamboUser, none: IDL.Null });
+  const PlaceBetResult = IDL.Variant({
+    win: IDL.Nat,
+    closed: IDL.Nat,
+    transferFailed: IDL.Text,
+    draw: IDL.Nat,
+    lose: IDL.Nat,
+    retry: IDL.Nat,
+  });
+  return IDL.Service({
+    blacklist: IDL.Func([IDL.Text], [IDL.Bool], []),
+    currentDevFee: IDL.Func([], [IDL.Nat], ["query"]),
+    getBList: IDL.Func([], [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Bool))], []),
+    getBalance: IDL.Func([IDL.Record({ te: IDL.Vec(IDL.Nat8) })], [Tokens], []),
+    getCounter: IDL.Func([], [IDL.Nat], ["query"]),
+    getCurrentGame: IDL.Func([], [GameData], []),
+    getCurrentIndex: IDL.Func([], [IDL.Nat], ["query"]),
+    getDevPool: IDL.Func([], [IDL.Principal], ["query"]),
+    getNextHalving: IDL.Func([], [IDL.Int], []),
+    getRewardPool: IDL.Func([], [IDL.Principal], ["query"]),
+    getUserBets: IDL.Func(
+      [IDL.Text],
+      [IDL.Variant({ ok: IDL.Vec(Bet), none: IDL.Nat })],
+      []
+    ),
+    isNotPaused: IDL.Func([], [IDL.Bool], ["query"]),
+    isNowSping: IDL.Func(
+      [],
+      [IDL.Record({ nes: IDL.Int, now: IDL.Int, res: IDL.Bool })],
+      []
+    ),
+    pauseCanister: IDL.Func([IDL.Bool], [IDL.Bool], []),
+    place_bet: IDL.Func([IDL.Nat, IDL.Nat], [PlaceBetResult], []),
+    setAdmin: IDL.Func([IDL.Principal], [IDL.Principal], []),
+    setDevPool: IDL.Func([IDL.Principal], [IDL.Principal], []),
+    setDuration: IDL.Func([IDL.Nat], [], []),
+    setEyesToken: IDL.Func([IDL.Bool], [IDL.Bool], []),
+    setRewardPool: IDL.Func([IDL.Principal], [IDL.Principal], []),
+    whoCall: IDL.Func([], [IDL.Principal], ["query"]),
+  });
+};
+export const init = ({ IDL }) => {
+  return [];
+};
