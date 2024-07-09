19513be38f79061b97b18f241e4b29716c101a21
rakafantino <rakafantino@gmail.com>
2024-07-10 03:07:35 +0700

    chore: changing home page menu, and adding link

---
+++ src/components/GameNode.jsx
@@ -0,0 +1,81 @@
+import { motion } from "framer-motion";
+import { useCallback, memo } from "react";
+import PropTypes from "prop-types";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { Link } from "react-router-dom";
+
+const GameNode = memo(({ node, animationValues }) => {
+  const handlePlayClick = useCallback(() => {
+    console.log(`Playing ${node.name}`);
+  }, [node.name]);
+
+  return (
+    <div className="flex flex-col items-center">
+      <motion.div
+        className="w-20 h-20 rounded-full bg-[#1E3557] bg-opacity-85 flex items-center justify-center cursor-pointer shadow-md hover:ring-4 hover:ring-[#F8B22A] hover:ring-opacity-78"
+        whileHover={{ scale: 1.1, boxShadow: "0 0 15px 5px rgba(248, 178, 42, 0.78)" }}
+      >
+        <motion.img
+          src={node.Icon}
+          className="text-3xl text-white glitch-effect"
+          alt={node.name}
+          animate={animationValues}
+          transition={{
+            duration: Math.random() * 0.5 + 0.3,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+        />
+      </motion.div>
+      {/* card info */}
+      <motion.div className="mt-4 bg-[#1E3557] bg-opacity-85 rounded-lg p-4 shadow-xl z-20 w-56" initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }}>
+        <div className="relative w-full h-40 mb-4">
+          <LazyLoadImage src={node.image} alt={node.name} className="w-full h-full object-cover rounded-lg" effect="blur" wrapperClassName="w-full h-full" />
+          <div className="absolute inset-x-0 top-0 bg-[#F8B22A] bg-opacity-85 flex items-center justify-center rounded-t-lg h-14">
+            <h3 className="text-white text-4xl font-bold font-passero">{node.name}</h3>
+          </div>
+        </div>
+        <p className="text-base mb-4 text-white">{node.description}</p>
+        {node.id === "dice" ? (
+          <>
+            <a href={node.navi}>
+              <button onClick={handlePlayClick} className="bg-[#EE5151] bg-opacity-85 text-white px-4 py-2 rounded-full w-full hover:bg-orange-600 transition-colors duration-200 shadow-md mb-2">
+                Play Dice Now
+              </button>
+            </a>
+            <a href={node.navi2}>
+              <button onClick={handlePlayClick} className="bg-[#EE5151] bg-opacity-85 text-white px-4 py-2 rounded-full w-full hover:bg-orange-600 transition-colors duration-200 shadow-md">
+                Play Mini Dice Now
+              </button>
+            </a>
+          </>
+        ) : (
+          <Link to={node.navi}>
+            <button onClick={handlePlayClick} className="bg-[#EE5151] bg-opacity-85 text-white px-4 py-2 rounded-full w-full hover:bg-orange-600 transition-colors duration-200 shadow-md">
+              Play Now
+            </button>
+          </Link>
+        )}
+      </motion.div>
+    </div>
+  );
+});
+
+GameNode.displayName = "GameNode";
+
+GameNode.propTypes = {
+  node: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    Icon: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    navi: PropTypes.string.isRequired,
+    navi2: PropTypes.string,
+  }).isRequired,
+  animationValues: PropTypes.object.isRequired,
+};
+
+export default GameNode;
