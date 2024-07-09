55f5add4831472e3782e7ae1a6edc9d017662910
icdragoneyes <icdragoneyes@gmail.com>
2024-07-09 22:56:43 +0700

    add mocking phrases

--- src/components/Roshambo/ResultOverlay.jsx
+++ src/components/Roshambo/ResultOverlay.jsx
@@ -13,18 +13,30 @@ const ResultOverlay = ({ userChoice, cpuChoice, onClose, icpWon }) => {
   const [chosenWinArray, setChosenWinArray] = useState("");
   const [chosenLoseArray, setChosenLoseArray] = useState("");
   const winArray = [
-    "Alright, you win for now",
-    "How could you senpai, im broke now",
-    "Please be easy on me :(",
-    "You defeated me, probly just newbie luck",
-    "Yamete, oni san",
+    "Alright, you win for now... but I'll be back!",
+    "How could you senpai, im broke now *sobs*", // Add dramatic flair
+    "Please be easy on me :( You're making me look bad!",
+    "You defeat me... but only because I underestimated you. This won't happen again!",
+    "Victory is yours... for a fleeting moment. I'll crush you next time!",
+    "You think you've won? Just wait until I unleash my secret weapon next time!",
+    "I let you win this time to avoid hurting your fragile ego. Don't get too cocky.",
+    "Yamete, oni san >_<",
+    "Your victory is nothing compared to the challenges I've faced. You're still a rookie.",
+    "I'm impressed... for a beginner. But don't expect this to happen again.",
+    "You've won the battle, but not the war. I'll be back for my revenge.",
   ];
+
   const loseArray = [
-    "Haha its 100 years too early to defeat me",
-    "Is that all you got?",
-    "Meh",
-    "I bet 'Losing' is your middle name?",
-    "Awww look who is crying now",
+    "Haha it seems 100 years isn't enough for you. Try 1000!",
+    "Is that all you got? That was pathetically easy.",
+    "Meh, is that the best you can do? Disappointing.",
+    "I bet 'Losing' is your middle name? It certainly suits you well.",
+    "Aw poor puppy, are you gonna cry about it? *wink*",
+    "I'm not even mad. I'm just disappointed. I had higher expectations of you.",
+    "You must have been practicing against babies. This is a real competition now.",
+    "I'm starting to think you're throwing the match. Is there something in it for you?",
+    "I'm not sure what's more embarrassing, your loss or your excuses.",
+    "Well, that was certainly... underwhelming. I'm not sure what I expected.",
   ];
 
   useEffect(() => {
