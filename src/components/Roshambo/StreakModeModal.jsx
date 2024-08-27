9065d8c6c3b5aa01eec393c14d46befe8a026e99
Raka Fantino <rakafantino@gmail.com>
2024-08-27 17:03:29 +0700

    feat: new wallet component called wallet2.jsx

--- src/components/Roshambo/StreakModeModal.jsx
+++ src/components/Roshambo/StreakModeModal.jsx
@@ -16,7 +16,7 @@ const StreakModeModal = ({ isOpen, onClose, streakMultiplier }) => {
         </div>
         <div className="flex justify-center items-center">
           <button onClick={onClose} className="mt-4 bg-red-500 text-white px-4 py-2 rounded">
-            Close
+            OK
           </button>
         </div>
       </div>
