50b02189ab8c2f631b0c8daedb41e5989d70fef7
icdragoneyes <icdragoneyes@gmail.com>
2024-09-01 01:31:52 +0700

    a

--- src/hooks/useTelegramWebApp.js
+++ src/hooks/useTelegramWebApp.js
@@ -104,7 +104,7 @@ const useTelegramWebApp = () => {
       const initData = telegramInitData;
       let url = baseUrlApi + "/api/auth";
       if (initData) {
-        var param = JSON.parse(webApp.initDataUnsafe);
+        var param = webApp.initDataUnsafe;
         try {
           const response = await axios.post(url, param, {
             headers: {
