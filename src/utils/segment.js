f524f9685a586fdd3576d9006cc3264d44e50b48
icdragoneyes <icdragoneyes@gmail.com>
2024-09-13 07:09:34 +0700

    add export default analytics

--- src/utils/segment.js
+++ src/utils/segment.js
@@ -1,3 +1,5 @@
 import { AnalyticsBrowser } from "@segment/analytics-next";
-
-export const analytics = AnalyticsBrowser.load({ writeKey: "4JmIdxFpYV45aYdHO8LGB0ygbyvdv3Qz" }).catch((err) => console.error(err));
+const analytics = AnalyticsBrowser.load({
+  writeKey: "4JmIdxFpYV45aYdHO8LGB0ygbyvdv3Qz",
+}).catch((err) => console.error(err));
+export default analytics;
