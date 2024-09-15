266765af60e84cb147850dd87855545473eec215
icdragoneyes <icdragoneyes@gmail.com>
2024-09-15 13:33:33 +0700

    update core function

--- src/IC/candid/core.did.js
+++ src/IC/candid/core.did.js
@@ -1,62 +1,101 @@
 export const idlFactory = ({ IDL }) => {
-  const FixedReward = IDL.Record({
-    'usd' : IDL.Float64,
-    'chain' : IDL.Text,
-    'gameName' : IDL.Text,
-    'amount' : IDL.Nat,
-    'decimal' : IDL.Nat,
-    'canisterid' : IDL.Text,
+  const Reward = IDL.Record({
+    usd: IDL.Float64,
+    chain: IDL.Text,
+    gameName: IDL.Text,
+    amount: IDL.Nat,
+    decimal: IDL.Nat,
+    canisterid: IDL.Text,
   });
-  const HttpHeader = IDL.Record({ 'value' : IDL.Text, 'name' : IDL.Text });
+  const HttpHeader = IDL.Record({ value: IDL.Text, name: IDL.Text });
   const HttpResponsePayload = IDL.Record({
-    'status' : IDL.Nat,
-    'body' : IDL.Vec(IDL.Nat8),
-    'headers' : IDL.Vec(HttpHeader),
+    status: IDL.Nat,
+    body: IDL.Vec(IDL.Nat8),
+    headers: IDL.Vec(HttpHeader),
   });
   const TransformArgs = IDL.Record({
-    'context' : IDL.Vec(IDL.Nat8),
-    'response' : HttpResponsePayload,
+    context: IDL.Vec(IDL.Nat8),
+    response: HttpResponsePayload,
   });
   const CanisterHttpResponsePayload = IDL.Record({
-    'status' : IDL.Nat,
-    'body' : IDL.Vec(IDL.Nat8),
-    'headers' : IDL.Vec(HttpHeader),
+    status: IDL.Nat,
+    body: IDL.Vec(IDL.Nat8),
+    headers: IDL.Vec(HttpHeader),
   });
   return IDL.Service({
-    'addAccess' : IDL.Func([IDL.Text], [IDL.Nat], []),
-    'getAllTotalReward' : IDL.Func(
-        [],
-        [IDL.Vec(IDL.Tuple(IDL.Text, FixedReward))],
-        ['query'],
-      ),
-    'getTotalReward' : IDL.Func(
-        [IDL.Text, IDL.Text],
-        [IDL.Record({ 'usd' : IDL.Float64, 'reward' : IDL.Float64 })],
-        ['query'],
-      ),
-    'mintEyes' : IDL.Func(
-        [IDL.Principal, IDL.Nat],
-        [IDL.Variant({ 'error' : IDL.Text, 'success' : IDL.Nat })],
-        [],
-      ),
-    'mintTestEyes' : IDL.Func(
-        [IDL.Principal, IDL.Nat],
-        [IDL.Variant({ 'error' : IDL.Text, 'success' : IDL.Nat })],
-        [],
-      ),
-    'outcall' : IDL.Func([IDL.Text], [IDL.Text], []),
-    'setATH' : IDL.Func([IDL.Text, IDL.Float64], [], []),
-    'transform' : IDL.Func(
-        [TransformArgs],
-        [CanisterHttpResponsePayload],
-        ['query'],
-      ),
-    'writeTotalReward' : IDL.Func(
-        [IDL.Nat, IDL.Text, IDL.Text, IDL.Nat],
-        [],
-        [],
-      ),
+    addAccess: IDL.Func([IDL.Text], [IDL.Nat], []),
+    applyCode: IDL.Func([IDL.Text], [IDL.Bool], []),
+    getAllTG: IDL.Func([], [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))], []),
+    getAllTotalReward: IDL.Func(
+      [],
+      [
+        IDL.Record({
+          id: IDL.Nat,
+          icp: IDL.Nat,
+          usdicp: IDL.Float64,
+          fullData: IDL.Vec(IDL.Tuple(IDL.Text, Reward)),
+        }),
+      ],
+      ["query"]
+    ),
+    getSOLwallet: IDL.Func([IDL.Text], [IDL.Text], ["query"]),
+    getTotalReward: IDL.Func(
+      [IDL.Text, IDL.Text],
+      [IDL.Record({ usd: IDL.Float64, reward: IDL.Float64 })],
+      ["query"]
+    ),
+    getUser: IDL.Func(
+      [],
+      [
+        IDL.Record({
+          userName: IDL.Text,
+          referralCode: IDL.Text,
+          friends: IDL.Vec(IDL.Text),
+        }),
+      ],
+      ["query"]
+    ),
+    getUserList: IDL.Func(
+      [],
+      [
+        IDL.Record({
+          referrer: IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+          users: IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+        }),
+      ],
+      []
+    ),
+    mintEyes: IDL.Func(
+      [IDL.Principal, IDL.Nat],
+      [IDL.Variant({ error: IDL.Text, success: IDL.Nat })],
+      []
+    ),
+    mintTestEyes: IDL.Func(
+      [IDL.Principal, IDL.Nat],
+      [IDL.Variant({ error: IDL.Text, success: IDL.Nat })],
+      []
+    ),
+    outcall: IDL.Func([IDL.Text], [IDL.Text], []),
+    setATH: IDL.Func([IDL.Text, IDL.Float64], [], []),
+    setUsername: IDL.Func([IDL.Text], [IDL.Text], []),
+    siwt: IDL.Func([IDL.Text, IDL.Text], [IDL.Text], []),
+    syncUserName: IDL.Func(
+      [],
+      [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))],
+      ["query"]
+    ),
+    testSetUsername: IDL.Func([IDL.Text, IDL.Text], [IDL.Text], []),
+    topUpLog: IDL.Func([IDL.Text], [IDL.Text], []),
+    transform: IDL.Func(
+      [TransformArgs],
+      [CanisterHttpResponsePayload],
+      ["query"]
+    ),
+    writeTotalReward: IDL.Func([IDL.Nat, IDL.Text, IDL.Text, IDL.Nat], [], []),
   });
 };
+
 // eslint-disable-next-line no-unused-vars
-export const init = ({ IDL }) => { return []; };
+export const init = ({ IDL }) => {
+  return [];
+};
