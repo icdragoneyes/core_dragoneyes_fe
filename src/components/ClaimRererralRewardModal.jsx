1e96e1d4061581a690fe21abd5d79df107a60c6c
icdragoneyes <icdragoneyes@gmail.com>
2024-09-17 16:21:45 +0700

    activate airdrop

--- src/components/ClaimRererralRewardModal.jsx
+++ src/components/ClaimRererralRewardModal.jsx
@@ -14,20 +14,40 @@ import {
 } from "../store/Atoms";
 
 const ClaimRererralRewardModal = () => {
-  const location = useLocation();
+  //const location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
   const [referrerUsername, setReferrerUsername] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [coreAgent] = useAtom(coreAtom);
+  const [code, setCode] = useState(false);
   //const [quota, setQuota] = useState(false);
   const [initData] = useAtom(telegramInitDataAtom);
   const [isAuthenticated] = useAtom(isAuthenticatedAtom);
+  const [success, setSuccess] = useState(1);
+  const [errmsg, setErrmsg] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+    var claimResult = await coreAgent.applyCode(code);
+    if (claimResult.success) {
+      setSuccess(2);
+    } else if (claimResult.codeinvalid) {
+      setSuccess(3);
+      setErrmsg("You got invalid referral code, please try another code");
+    } else if (claimResult.quotaexceeded) {
+      setSuccess(3);
+      setErrmsg(
+        "Awww snap, you are too late, the quota for this referral code is exceeded. You can try again next Monday, or find another referral link"
+      );
+    } else if (claimResult.referred) {
+      setSuccess(3);
+      setErrmsg("Cannot claim, you have already been referred");
+    }
+    setIsLoading(false);
+  };
+
+  const closeAirdropModal = async () => {
+    setIsOpen(false);
   };
 
   useEffect(() => {
@@ -37,6 +57,23 @@ const ClaimRererralRewardModal = () => {
         var referralData = await coreAgent.getCodeData(rcode);
         if (referralData.result) {
           setReferrerUsername(referralData.result.referrerUsername);
+          var claimResult = referralData.result.data;
+          if (claimResult.success) {
+            setSuccess(1);
+          } else if (claimResult.codeinvalid) {
+            setSuccess(3);
+            setErrmsg("You got invalid referral code, please try another code");
+          } else if (claimResult.quotaexceeded) {
+            setSuccess(3);
+            setErrmsg(
+              "Awww snap, you are too late, the quota for this referral code is exceeded. You can try again tomorrow, or find another referral link"
+            );
+          } else if (claimResult.referred) {
+            setSuccess(3);
+            setErrmsg(
+              "Cannot claim using this code, as you have already been referred"
+            );
+          }
           setIsOpen(true);
         }
       } else {
@@ -57,6 +94,7 @@ const ClaimRererralRewardModal = () => {
       if (rc) {
         console.log("Referral Code:", rc);
         referralCodeValue = rc;
+        setCode(rc);
         // You can now use the referralCode in your app logic
       } else {
         console.log("No referral code found");
@@ -78,27 +116,62 @@ const ClaimRererralRewardModal = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       {/* Background Overlay */}
-      <div className="relative w-10/12 h-4/6 bg-[#343433FA] flex items-center justify-start flex-col gap-4 rounded-lg shadow-lg p-6 z-50">
-        <p className="font-passion text-[40px] text-[#E8A700]">
-          Congratulations!
-        </p>
-        <p className="font-passion text-[24px] text-white w-8/12 text-center">
-          {`You got 0.03 SOL from ${referrerUsername}`}
-        </p>
-        <img src={SolReceived} className="mt-5" />
+      {success == 1 && (
+        <div className="relative w-10/12 h-4/6 bg-[#343433FA] flex items-center justify-start flex-col gap-4 rounded-lg shadow-lg p-6 z-50">
+          <p className="font-passion text-[40px] text-[#E8A700]">
+            Congratulations!
+          </p>
+          <p className="font-passion text-[24px] text-white w-8/12 text-center">
+            {`You got 0.03 SOL from ${referrerUsername}`}
+          </p>
+          <img src={SolReceived} className="mt-5 w-[150px]" />
 
-        <button
-          onClick={handleSubmit}
-          className={`px-8 py-2 bg-blue-500 text-white rounded-lg flex items-center justify-center transition-all duration-300 font-passion ${
-            isLoading ? "cursor-not-allowed opacity-70" : "hover:bg-blue-600"
-          }`}
-          disabled={isLoading}
-        >
-          <span className="flex items-center">
-            {isLoading ? "Loading..." : "Claim Now!"}
-          </span>
-        </button>
-      </div>
+          <button
+            onClick={handleSubmit}
+            className={`px-8 py-2 bg-blue-500 text-white rounded-lg flex items-center justify-center transition-all duration-300 font-passion ${
+              isLoading ? "cursor-not-allowed opacity-70" : "hover:bg-blue-600"
+            }`}
+            disabled={isLoading}
+          >
+            <span className="flex items-center">
+              {isLoading ? "Loading..." : "Claim Now!"}
+            </span>
+          </button>
+        </div>
+      )}
+      {success == 2 && (
+        <div className="relative w-10/12 h-4/6 bg-[#343433FA] flex items-center justify-start flex-col gap-4 rounded-lg shadow-lg p-6 z-50">
+          <p className="font-passion text-[40px] text-[#E8A700]">AWYISSS</p>
+          <p className="font-passion text-[24px] text-white w-8/12 text-center"></p>
+          Claim successful, 0.03 SOL is yours! Now lets go play some games!
+          <button
+            onClick={closeAirdropModal}
+            className={`px-8 py-2 bg-blue-500 text-white rounded-lg flex items-center justify-center transition-all duration-300 font-passion ${
+              isLoading ? "cursor-not-allowed opacity-70" : "hover:bg-blue-600"
+            }`}
+            disabled={isLoading}
+          >
+            <span className="flex items-center">OK</span>
+          </button>
+        </div>
+      )}
+      {success == 3 && (
+        <div className="relative w-10/12 h-4/6 bg-[#343433FA] flex items-center justify-start flex-col gap-4 rounded-lg shadow-lg p-6 z-50">
+          <p className="font-passion text-[24px] text-white w-8/12 text-center">
+            {errmsg}
+          </p>
+
+          <button
+            onClick={closeAirdropModal}
+            className={`px-8 py-2 bg-blue-500 text-white rounded-lg flex items-center justify-center transition-all duration-300 font-passion ${
+              isLoading ? "cursor-not-allowed opacity-70" : "hover:bg-blue-600"
+            }`}
+            disabled={isLoading}
+          >
+            <span className="flex items-center">OK</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
