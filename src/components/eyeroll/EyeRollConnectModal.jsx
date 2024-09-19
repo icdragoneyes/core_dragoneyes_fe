9e9bb7e13ff1a983d762d1e6e6e457cd8cded2c1
icdragoneyes <icdragoneyes@gmail.com>
2024-09-19 08:30:43 +0700

    change sleeping dragon caption

--- src/components/eyeroll/EyeRollConnectModal.jsx
+++ src/components/eyeroll/EyeRollConnectModal.jsx
@@ -4,7 +4,11 @@ import eyeClose from "../../assets/eyeroll/eye-close.png";
 import eyeOpenVid from "../../assets/eyeroll/eye-open-vid.mp4";
 import eyeOpen from "../../assets/eyeroll/eye-open.jpg";
 import { useAtom } from "jotai";
-import { isAuthenticatedAtom, walletAddressAtom, hasSeenSplashScreenAtom } from "../../store/Atoms";
+import {
+  isAuthenticatedAtom,
+  walletAddressAtom,
+  hasSeenSplashScreenAtom,
+} from "../../store/Atoms";
 
 const EyeRollConnectModal = ({ onComplete }) => {
   const [stage, setStage] = useState("initial");
@@ -13,7 +17,9 @@ const EyeRollConnectModal = ({ onComplete }) => {
   const eyeOpenVideoRef = useRef(null);
   const [isAuthenticated] = useAtom(isAuthenticatedAtom);
   const [walletAddress] = useAtom(walletAddressAtom);
-  const [hasSeenSplashScreen, setHasSeenSplashScreen] = useAtom(hasSeenSplashScreenAtom);
+  const [hasSeenSplashScreen, setHasSeenSplashScreen] = useAtom(
+    hasSeenSplashScreenAtom
+  );
 
   useEffect(() => {
     const hasSeenSplash = sessionStorage.getItem("hasSeenSplashScreen");
@@ -68,16 +74,26 @@ const EyeRollConnectModal = ({ onComplete }) => {
   if (hasSeenSplashScreen) return null;
 
   return (
-    <div className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-1000 ${fadeOut ? "opacity-0" : "opacity-100"}`}>
+    <div
+      className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-1000 ${
+        fadeOut ? "opacity-0" : "opacity-100"
+      }`}
+    >
       <div className="relative w-full h-full overflow-hidden">
-        <img src={eyeClose} alt="Sleeping Dragon" className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${stage === "initial" ? "opacity-100" : "opacity-0"}`} />
+        <img
+          src={eyeClose}
+          alt="Sleeping Dragon"
+          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${
+            stage === "initial" ? "opacity-100" : "opacity-0"
+          }`}
+        />
 
         {stage === "initial" && (
           <div className="absolute inset-0 flex items-center justify-center ">
-            <div className="bg-black bg-opacity-70 p-6 rounded-lg text-white text-center font-passion w-4/5">
+            <div className="bg-black bg-opacity-70 p-6 mt-15 rounded-lg text-white text-center font-passion w-4/5">
               <p className="text-xl mb-4">
-                Connecting to dragon onchain <br />
-                system{dots}
+                Awaking The Dragon.. <br />
+                {dots}
               </p>
             </div>
           </div>
@@ -89,10 +105,18 @@ const EyeRollConnectModal = ({ onComplete }) => {
           muted
           playsInline
           onEnded={handleVideoEnd}
-          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${stage === "videoPlaying" ? "opacity-100" : "opacity-0"}`}
+          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${
+            stage === "videoPlaying" ? "opacity-100" : "opacity-0"
+          }`}
         />
 
-        <img src={eyeOpen} alt="Dragon Eye Open" className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${stage === "eyeOpen" ? "opacity-100" : "opacity-0"}`} />
+        <img
+          src={eyeOpen}
+          alt="Dragon Eye Open"
+          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${
+            stage === "eyeOpen" ? "opacity-100" : "opacity-0"
+          }`}
+        />
       </div>
     </div>
   );
