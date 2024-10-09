5b868cc87087f172cc90327c0e0dd2c864589d30
Raka Fantino <rakafantino@gmail.com>
2024-10-09 13:17:47 +0700

    try pulse effect in streak mode

--- src/components/Roshambo/PulseEffect.jsx
+++ src/components/Roshambo/PulseEffect.jsx
@@ -6,16 +6,16 @@ const PulseEffect = ({ show }) => {
   return (
     <>
       {/* Vignette effect */}
-      <div className="fixed inset-0 pointer-events-none z-40 bg-gradient-radial from-transparent to-black opacity-50"></div>
+      <div className="fixed inset-0 pointer-events-none z-40 bg-black bg-opacity-50"></div>
 
       {/* Left pulse */}
-      <div className="fixed left-0 top-1/2 -translate-y-1/2 w-16 h-64 z-50 overflow-hidden">
-        <div className="absolute -left-16 top-1/2 -translate-y-1/2 w-32 h-32 rounded-full bg-gradient-radial from-green-500 to-transparent opacity-50 animate-pulse2"></div>
+      <div className="fixed left-0 top-1/2 -translate-y-1/2 w-12 h-48 z-50 overflow-hidden">
+        <div className="absolute -left-12 top-1/2 -translate-y-1/2 w-24 h-24 rounded-full bg-gradient-to-r from-green-500 to-transparent opacity-50 animate-pulse2"></div>
       </div>
 
       {/* Right pulse */}
-      <div className="fixed right-0 top-1/2 -translate-y-1/2 w-16 h-64 z-50 overflow-hidden">
-        <div className="absolute -right-16 top-1/2 -translate-y-1/2 w-32 h-32 rounded-full bg-gradient-radial from-green-500 to-transparent opacity-50 animate-pulse2"></div>
+      <div className="fixed right-0 top-1/2 -translate-y-1/2 w-12 h-48 z-50 overflow-hidden">
+        <div className="absolute -right-12 top-1/2 -translate-y-1/2 w-24 h-24 rounded-full bg-gradient-to-l from-green-500 to-transparent opacity-50 animate-pulse2"></div>
       </div>
     </>
   );
